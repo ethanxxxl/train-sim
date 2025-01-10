8851c5f4f4311694fb4170c3250a2f34a7636451
Ethan Smith <ethansmith.dev@gmail.com>
2025-01-09 22:36:24 -0500

    A lot of stuff is working.
    SDL2 now has a RenderGeometry function.  It didn't have bindings in cl-sdl2, so
    I created an extension to support the function.  Using this function, I am
    drawing lines of variable thickness (and angle), n-gons, and n-gon shells.

--- main.lisp
+++ main.lisp
@@ -1,5 +1,6 @@
 (require :sdl2)
 (require :sdl2-gfx)
+(load "./sdl-vector.lisp")
 
 (defstruct station
            "describes a train station that trains may connect to."
@@ -48,13 +49,112 @@ number of points."
   (sdl2:set-render-draw-color renderer 0 0 0 255)
   (sdl2:render-clear renderer))
 
-(defstruct test-object
-  surface t)
+(defvar *line-color* (list 121 219 72 255))
+(defun set-line-color (r g b &optional (a 255))
+  (setf *line-color* (list r g b a)))
 
+(defvar *print-p* nil)
+(defun draw-vector-line (renderer x1 y1 x2 y2 &optional (thick 1))
+  (let* ((atan-w (atan (- x1 x2) (- y1 y2)))
+         (x-offset (- (* thick 0.5 (cos atan-w))))
+         (y-offset (* thick 0.5 (sin atan-w)))
+         (xa (+ x1 x-offset))
+         (ya (+ y1 y-offset))
+         (xb (- x1 x-offset))
+         (yb (- y1 y-offset))
+
+         (xc (+ x2 x-offset))
+         (yc (+ y2 y-offset))
+         (xd (- x2 x-offset))
+         (yd (- y2 y-offset)))
+    
+    (sdl2:with-verts ((verts num
+                             (list xa ya
+                                       xb yb
+                                       xc yc
+                                       xd yd)
+                             *line-color*))
+      (sdl2:with-indices ((indices idx-len '(0 1 2
+                                             2 3 1)))
+        (sdl2:render-geometry renderer nil verts num indices idx-len)))))
+
+(defun ngon-points (x y n rad &optional rot-degrees)
+  (let ((rot-radians (* pi (/ (or rot-degrees 0) 180))))
+    (loop with ret = (list)
+          repeat n
+          for theta from rot-radians by (/ (* 2 pi) n)
+          do (progn (push (coerce (+ y (* rad (sin theta))) 'single-float) ret)
+                    (push (coerce (+ x (* rad (cos theta))) 'single-float) ret))
+          finally (return ret))))
+
+(defun ngon-indices (n)
+  (loop with indices = (list 0 1 2)
+        for i from 3
+        repeat (- n 3)
+        do (setf indices (append indices (list (- i 1) 0 i)))
+        finally (return indices)))
+
+(defun ngon-shell-indices (n)
+  (declare (optimize (debug 3)))
+  (append (apply 'append
+                 (loop for s from 0 below (1- n)
+                       collect (list s (1+ s) (+ n s) (1+ s) (+ n s) (+ n s 1))))
+          (list (1- n) 0 (+ n n -1) 0 (+ n n -1) n)))
+
+(defun draw-ngon (renderer x y n rad &optional rot-degrees)
+  (sdl2:with-verts ((verts num (ngon-points x y n rad rot-degrees) *line-color*))
+    (sdl2:with-indices ((indices idx-len (ngon-indices n)))
+      (sdl2:render-geometry renderer nil
+                            verts num
+                            indices idx-len))))
+
+(defun draw-ngon-shell (renderer x y n rad thick &optional rot-degrees)
+  "rad - thickness must be greater than zero"
+  (sdl2:with-verts ((verts vert-len
+                           (append (ngon-points x y n rad rot-degrees)
+                                   (ngon-points x y n (- rad thick) rot-degrees))
+                           *line-color*))
+    (sdl2:with-indices ((indices idx-len (ngon-shell-indices n)))
+      (sdl2:render-geometry renderer nil
+                            verts vert-len
+                            indices idx-len))))
+
+(defun draw-sloped-line (renderer m b &optional (thick 2))
+  (let ((y1 (+ (* (- m) -400) (- b) 300))
+        (y2 (+ (* (- m) 400) (- b) 300)))
+    (draw-vector-line renderer 0 y1 800 y2 thick)))
+
+(defvar *mouse-x* 1)
+(defvar *mouse-y* 1)
+(defvar *ngon* 32)
+(defvar *ngon-rad* 100)
+(defvar *ngon-rot* 0)
 (defun draw-test-object (renderer)
-  ;(sdl2-gfx:rounded-rectangle-color )
-  ;(sdl2-gfx:rotozoom-surface)
-  (sdl2-gfx::aacircle-rgba renderer 250 100 50 100 230 100 255))
+  (set-line-color 25 40 25)
+  (loop for y from 0 to 600 by 50
+        do (draw-vector-line renderer 0 y 800 y 1.0))
+
+    (loop for x from 0 to 800 by 50
+          do (draw-vector-line renderer x 0 x 600 1.0))
+
+  (draw-vector-line renderer 0 300 800 300 2)
+  (draw-vector-line renderer 400 0 400 600 2)
+
+  (let ((x (- *mouse-x* 400))
+        (y (- (- *mouse-y* 300))))
+    (if (and (/= x 0) (/= y 0))
+        (let* ((m (/ y x))
+               (w (- (/ x y))))
+
+          (set-line-color 26 219 122)
+          (draw-sloped-line renderer m 0 10)
+          
+          (set-line-color 26 219 26)
+          (draw-sloped-line renderer w (- y (* w x)) 1)
+
+          (set-line-color 247 17 18)
+          (draw-ngon-shell renderer *mouse-x* *mouse-y* *ngon* (* 10 *ngon-rad*) 1 (* 5 (/ *ngon-rot* 45)))))))
+
 
 (defvar *run-program* nil
   "set to true during main function, when threads are created and graphics is
@@ -63,19 +163,52 @@ started.")
 (defvar *renderer* nil "variable for debugging purposes")
 (defvar +screen-width+ 640)
 (defvar +screen-height+ 480)
+(defvar *ctrl-key* nil "set to true while ctrl key is pressed")
+(defvar *shift-key* nil "set to true while shift key is pressed")
+
 (defun gfx-loop-thread ()
 "use SDL to create a graphics window and draw stuff"
   (let ((station (make-station :name "test-station" :x 50 :y 50)))
     ;; initialization
     (sdl2:with-init (:everything)
+      ;; enable Anti-Aliasing
+      (sdl2-ffi.functions:sdl-gl-set-attribute
+       sdl2-ffi:+sdl-gl-multisamplesamples+ 4)
+      
       (sdl2:with-window (win :title "SDL2 from Common Lisp" :flags '(:shown))
         (sdl2:with-renderer (renderer win :flags '(:accelerated))
           (setf *renderer* renderer)
           (sdl2:with-event-loop (:method :poll)
+            (:keydown
+             (:keysym keysym)
+             (alexandria:switch ((sdl2:scancode-value keysym)
+                                 :test 'sdl2:scancode=)
+               (:scancode-lctrl (setf *ctrl-key* t))
+               (:scancode-lshift (setf *shift-key* t))))
             (:keyup
              (:keysym keysym)
-             (when (sdl2:scancode= (sdl2:scancode-value keysym) :scancode-escape)
-               (sdl2:push-event :quit)))
+             (alexandria:switch ((sdl2:scancode-value keysym)
+                                 :test 'sdl2:scancode=)
+               (:scancode-escape (sdl2:push-event :quit))
+               (:scancode-lctrl (setf *ctrl-key* nil))
+               (:scancode-lshift (setf *shift-key* nil))))
+            (:mousebuttondown ()
+             (setf *print-p* t))
+            (:mousebuttonup
+             ()
+             (setf *print-p* nil))
+            (:mousemotion
+             (:x x :y y :state state)
+             (when (= state 1)
+               (setf *mouse-x* x
+                     *mouse-y* y)))
+            (:mousewheel
+             (:y scroll)
+             (cond
+               (*ctrl-key* (setf *ngon* (max 3 (+ *ngon* scroll))))
+               (*shift-key* (setf *ngon-rot* (+ *ngon-rot* (* 45 scroll))))
+               (t (setf *ngon-rad* (+ *ngon-rad* scroll))))
+             )
             (:idle
              ()
              (gfx-clear renderer)
@@ -86,7 +219,7 @@ started.")
              (draw-test-object renderer)
                           
              (sdl2:render-present renderer)
-             (sdl2:delay 33)
+             (sdl2:delay 30)
              
              (unless *run-program*
                (sdl2:push-event :quit)))
@@ -118,9 +251,16 @@ started.")
     (setf *gfx-thread* nil)))
 
 (defun start-bg-gfx ()
-  (setf *run-program* t
-        *gfx-thread* (sb-thread:make-thread #'gfx-loop-thread)))
+  "start a new graphic thread, unless one is already running"
+  (unless (and *gfx-thread* (sb-thread:thread-alive-p *gfx-thread*))
+    (setf *run-program* t
+          *gfx-thread* (sb-thread:make-thread #'gfx-loop-thread :name "Graphics Test"))))
 
 (defun join-bg-gfx ()
   (setf *run-program* nil)
   (sb-thread:join-thread *gfx-thread*))
+
+(defun cleanup-broken-gfx-thread ()
+  (sb-thread:terminate-thread *gfx-thread*)
+  (setf *run-program* nil))
+  
---
+++ sdl-vector.lisp
@@ -0,0 +1,153 @@
+;;; Author: Ethan Smith
+
+;;; This is an extension to the SDL2 package, and provides support for the
+;;; SDL_RenderGeometry funciton, along with facilities for generating vertices
+;;; and indices.
+
+(in-package :sdl2)
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;; Vertices
+
+(defun make-vertices (num)
+  "allocates a c-array of sdl-vertex with length num.  This needs to be free'd.
+Element access through c-ref is recommended."
+  (c-let ((verts sdl2-ffi:sdl-vertex :count num)) verts))
+
+(defun make-vertices (positions &optional colors tex-coords)
+  "allocates a c-array of sdl-vertex.  The number of vectors produced is determined
+by longest number of entries."
+  (if (and (= 0 (mod (length positions) 2))
+           (= 0 (mod (length colors) 4))
+           (= 0 (mod (length tex-coords) 2))) 
+
+      ;; `colors' has twice as many elements per entry than either `positions' or
+      ;; `tex-coords'
+      (let ((len (max (/ (length colors) 4)
+                      (/ (length positions) 2)
+                      (/ (length tex-coords) 2)))
+
+            ;; vertex default values
+            (x 0.0) (y 0.0)
+            (r 255) (g 255) (b 255) (a 255)
+            (u 0.0) (v 0.0))
+
+        (c-let ((verts sdl2-ffi:sdl-vertex :count len))
+          (loop
+            with pos = positions
+            with color = colors
+            with tex = tex-coords
+
+            for i from 0 below len
+
+            do (progn
+                 (setf x (float (or (nth 0 pos) x))
+                       y (float (or (nth 1 pos) y))
+
+                       u (float (or (nth 0 tex) u))
+                       v (float (or (nth 1 tex) v))
+                       
+                       r (or (nth 0 color) r)
+                       g (or (nth 1 color) g)
+                       b (or (nth 2 color) b)
+                       a (or (nth 3 color) a))
+
+                 (setf (verts i :position :x) x
+                       (verts i :position :y) y
+                       
+                       (verts i :tex-coord :x) u
+                       (verts i :tex-coord :y) v
+
+                       (verts i :color :r) r
+                       (verts i :color :g) g
+                       (verts i :color :b) b
+                       (verts i :color :a) a)
+
+                 (setf pos (nthcdr 2 pos)
+                       color (nthcdr 4 colors)
+                       tex (nthcdr 2 tex-coords))))
+
+          (values verts len)))
+      (error "positions, colors, and tex-coords must be multiples of 2,4, or 2")))
+
+(defun free-vertices (verts)
+  (cffi:foreign-free (ptr verts))
+  (autowrap:invalidate verts))
+
+(defmacro %with-verts ((vertex-ptr num-verts positions &optional colors tex-coords)
+                       &body body)
+  `(unwind-protect  
+        (multiple-value-bind (,vertex-ptr ,num-verts)
+            (make-vertices ,positions ,colors ,tex-coords)
+          (unwind-protect (progn ,@body)
+            (free-vertices ,vertex-ptr)))))
+
+(defmacro with-verts (bindings &body body)
+  "bindings is a list of the forms in the following format: (vert-ptr-var len-var
+positions &optional colors tex-coords).  The `positions' `colors' and
+`tex-coords' variables will be passed to `make-vertices' and its results bound
+to `vert-ptr-var' and `len-var'.  Like a LET binding, `positions' `colors' and
+`tex-coords' are evaluated.  All allocated resources are free'd after `body' is
+evaluated.'"
+  (if bindings
+      `(%with-verts ,(car bindings)
+         (with-verts ,(cdr bindings) ,@body))
+      `(progn ,@body)))
+
+(defun print-vertices (vertex-ptr num-verts)
+  (c-with ((verts sdl2-ffi:sdl-vertex :from vertex-ptr))
+    (loop for n from 0 below num-verts
+          do (format t "~S] x,y: ~S,~S | rgba: ~S,~S,~S,~S | u,v: ~S,~S~%"
+                     n
+                     (verts n :position :x)
+                     (verts n :position :y)
+                     (verts n :color :r)
+                     (verts n :color :g)
+                     (verts n :color :b)
+                     (verts n :color :a)
+                     (verts n :tex-coord :x)
+                     (verts n :tex-coord :y)))))
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;; Indices 
+(defun make-index-ptr (&rest indices)
+  (let ((len (length indices)))
+    (c-let ((index-ptr :int :count len))
+      (loop for idx in indices
+            for i from 0
+            do (setf (index-ptr i) idx))
+      (index-ptr &))))
+
+(defun free-index-ptr (index-ptr)
+  (cffi:foreign-free index-ptr))
+
+(defmacro %with-indices ((idx-var len-var bind-val) &body body)
+  `(let ((,idx-var (apply 'make-index-ptr ,bind-val))
+         (,len-var (length ,bind-val)))
+     (unwind-protect (progn ,@body)
+       (free-index-ptr ,idx-var))))
+
+(defmacro with-indices (bindings &body body)
+  "`bindings' is in the form ((ptr-var1 len-var1 indices) (ptr2-val2 len-var2
+indices) ...), and each variable is bound to an integer array containing the
+indices, and `body' is executed in this scope.  Just as in a LET form, the
+bind-value is evaluated.  All allocated resources are free'd after `body' is
+executed."
+  (if bindings
+      `(%with-indices ,(car bindings)
+         (with-indices ,(cdr bindings) ,@body))
+      `(progn ,@body)))
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;; Geometry
+
+(defun render-geometry (renderer
+                        texture
+                        vertices num-vertices
+                        indices num-indices)
+  (check-rc (sdl-render-geometry renderer
+                                 texture
+                                 vertices num-vertices
+                                 indices num-indices)))
+
+(export (list 'render-geometry 'with-verts 'with-indices 'print-vertices) :sdl2)
